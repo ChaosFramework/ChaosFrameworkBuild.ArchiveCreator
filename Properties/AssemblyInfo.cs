9f4f77ac4a4ff3e140a9ab3b1e20455ddc0ff2e3
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2025-10-04 17:53:36 +0200

    add code

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChaosFrameworkBuild.ArchiveCreator")]
+[assembly: AssemblyProduct("ChaosFrameworkBuild.ArchiveCreator")]
+[assembly: AssemblyCompany("ChaosTechnology")]
+[assembly: AssemblyCopyright("Copyright © ChaosTechnology 2025")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
